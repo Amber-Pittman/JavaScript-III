abbac686d464ef520741836de3a526210f87516d
Amber Pittman <amber@amberpittman.com>
2019-04-23 19:48:12 -0400

    Answered Principles 1&2. Created Code example for both.

--- assignments/this.js
+++ assignments/this.js
@@ -1,8 +1,10 @@
 /* The four principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
+* 1. During Global Binding, the value of `this` will be the Window/Console Object. For example,
+ the console output will have `Window {postMessage: f, blur: f, ...}`.
+* 2. Whenever a function is called by a preceding dot `.`, the object to the left of the dot gets
+`this`.
 * 3. 
 * 4. 
 *
@@ -10,12 +12,24 @@
 */
 
 // Principle 1
-
 // code example for Window Binding
+function aGirlHasNo(name) {
+    console.log(this);
+    return name;
+}
+aGirlHasNo("Name");
 
 // Principle 2
-
 // code example for Implicit Binding
+
+const yourObj = {
+    sing: "Hello",
+    singHello: function(name) {
+        console.log(`${this.sing} is it ${name} you're looking for?`);
+        console.log(this);
+    }
+};
+yourObj.singHello("me");
 
 // Principle 3
 
